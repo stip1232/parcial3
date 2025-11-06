fc13c0b5a3b7b2869a5f98af8cf8fd5f6fc43b05
stip1232 <santipena2207@gmail.com>
2025-11-05 23:40:53 -0500

    Add files via upload

--- animal.py
+++ animal.py
@@ -1,1 +1,18 @@
 
+# animal.py
+
+class Animal:
+    def __init__(self, codigo, raza, edad):
+        self.codigo = codigo
+        self.raza = raza
+        self.edad = edad
+        self.huevos_semana = 0
+
+    def registrar_produccion(self, cantidad):
+        self.huevos_semana += cantidad
+
+    def reiniciar_produccion(self):
+        self.huevos_semana = 0
+
+    def __str__(self):
+        return f"Código: {self.codigo}, Raza: {self.raza}, Edad: {self.edad} años, Huevos/Semana: {self.huevos_semana}"
